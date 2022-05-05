2fa15bd69fc75fd15346feb70e19fdc57026fd85
Wandeson027 <w.rafael.s@hotmail.com>
2022-05-05 15:53:50 -0300

    model & controller

--- src/main/java/com/example/crudspring/controller/CourseController.java
+++ src/main/java/com/example/crudspring/controller/CourseController.java
@@ -1,15 +1,18 @@
 package com.example.crudspring.controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/hello")
-public class HelloController {
- 
+@RequestMapping("/api/courses")
+public class CourseController {
+
     @GetMapping
-    public String hello(){
-        return "Hello!";
+    public List<Object> list(){
+        return null;
     }
+
 }
