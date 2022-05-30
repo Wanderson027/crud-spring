5358b0461cd430a791c848063f32dbf7e5599d2c
Wandeson027 <w.rafael.s@hotmail.com>
2022-05-30 15:09:06 -0300

    alterado o metodo post

--- src/main/java/com/example/crudspring/controller/CourseController.java
+++ src/main/java/com/example/crudspring/controller/CourseController.java
@@ -7,10 +7,8 @@ import com.example.crudspring.model.Course;
 import com.example.crudspring.repository.CourseRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/courses")
@@ -24,4 +22,13 @@ public class CourseController {
         return courseRepository.findAll();
     }
 
+    @PostMapping
+    public Course create(@RequestBody Course course) {
+
+        return courseRepository.save(course);
+    }
+
+//    @RequestMapping(value="/{id}", method = RequestMethod.DELETE)
+//    public ResponseEntity<Void> delete(@PathVariable Integer id){
+//    }
 }
